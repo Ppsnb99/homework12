08fce072bf07156fd572b849a357032e3d4f8020
siriporn nabangchang <nabangchang_s2@silpakorn.edu>
2022-03-19 22:07:29 +0700

    Initial commit

---
+++ lib/game.dart
@@ -0,0 +1,34 @@
+//ignor_for_flie: avoid_print
+
+import 'dart:math';
+
+class Game {
+  static const defaultMaxRandom = 100;
+  int? _answer;
+  int _guessCount = 0;
+  static final List<int> guessCountList = [];
+
+  Game({int maxRandom = defaultMaxRandom}) {
+    var r = Random();
+    _answer = r.nextInt(maxRandom) + 1;
+  }
+
+  int get guessCount {
+    return _guessCount;
+  }
+
+  void addCountList() {
+    guessCountList.add(_guessCount);
+  }
+
+  int doGuess(int num){
+    _guessCount++;
+    if(num > _answer!){
+      return 1;  //too high return 1
+    }else if (num < _answer!){
+      return -1;  //too low return -1
+    }else{
+      return 0;  //ทายถูก return 0
+    }
+  }
+}
